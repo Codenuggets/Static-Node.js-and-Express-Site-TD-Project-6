5cef7d839a65884124392cc566a228db1f802a95
Codenuggets <wd14link@gmail.com>
2019-08-17 21:49:13 -0400

    Added images

--- app.js
+++ app.js
@@ -9,17 +9,15 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
-  const {description} = projects[0];
-  console.log(description);
-  res.render('index', {description: description});
+  res.render('index', { projects });
 });
 
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.get('/:id', (req, res) => {
-  const {id, project_name, description, technologies, live_link, github_link, image_urls} = projects[req.params.id];
+app.get('/projects/:id', (req, res) => {
+  const { id, project_name, description, technologies, live_link, github_link, image_urls } = projects[req.params.id];
   res.render('project', {id, project_name, description, technologies, live_link, github_link, image_urls});
 })
 
